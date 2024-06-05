d526aec7fd5a87ecebab6838329058cbf550b076
Caleb O'Neal <caleboneal07@gmail.com>
2024-06-05 13:53:26 -0400

    Add some stuff to education

--- src/routes/education/index.tsx
+++ src/routes/education/index.tsx
@@ -1,10 +1,24 @@
 import { component$ } from "@builder.io/qwik";
 import Pageheader from "~/components/pageheader/pageheader";
+import styles from "./index.module.css";
 
 export default component$(() => {
   return (
     <>
       <Pageheader name="Education"></Pageheader>
+      <div class={styles.center}>
+        <h2> Northern Virginia Community College</h2>
+        <img
+          src="https://www.nvcc.edu/dist/images/sites/about/locations/annandale/annandale-campus-for-homepage.jpg"
+          width={700}
+          height={500}
+        />
+        <h4>Courses</h4>
+        <ul>
+          <li>Calculus I</li>
+          <li>Calculus II</li>
+        </ul>
+      </div>
     </>
   );
 });
