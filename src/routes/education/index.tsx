5d5a44dc7b84ae5b8a09dc539c2c8bb7617b779a
Caleb O'Neal <caleboneal07@gmail.com>
2024-06-05 11:57:03 -0400

    Fix error

--- src/routes/education/index.tsx
+++ src/routes/education/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import Pageheader from "~/components/pageheader/pageheader";
-import styles from "./index.module.css";
 
 export default component$(() => {
   return (
